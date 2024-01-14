38f3e4ed3b0df0e314e6198313ecf68fa99fd553
Johan Eliasson <johan@eliasson.me>
2024-01-14 16:10:29 +0100

    x

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # pg-nanoid
+
+## Get started
+
+Execute the code in [`nanoid.sql`](blob/main/nanoid.sql). This will create the `nanoid()` function in Postgres for your to use.
+
+Use the `nanoid()` function wherever you want a nano id.
+
+## Example
+
+```sql
+CREATE TABLE customers(
+  id serial PRIMARY KEY,
+  public_id text NOT NULL UNIQUE CHECK (public_id LIKE 'cus_%') DEFAULT nanoid(8, '023456789abcdefghjkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ', 1.6, 'cus_'),
+  name text NOT NULL
+);
+```
--- nanoid.sql
+++ nanoid.sql
@@ -28,93 +28,74 @@
  *
  * This modified version is also distributed under the Apache License, Version 2.0.
  */
-
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 DROP FUNCTION IF EXISTS nanoid(int, text, float, text);
-CREATE OR REPLACE FUNCTION nanoid(
-    size int DEFAULT 21, 
-    alphabet text DEFAULT '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 
-    additionalBytesFactor float DEFAULT 1.6,
-    prefix text DEFAULT '' -- New parameter for the prefix
-)
-    RETURNS text 
+
+CREATE OR REPLACE FUNCTION nanoid(prefix text DEFAULT '', size int DEFAULT 21, alphabet text DEFAULT '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', additionalBytesFactor float DEFAULT 1.6)
+    RETURNS text
     LANGUAGE plpgsql
-    VOLATILE
-    LEAKPROOF
-    PARALLEL SAFE
-AS
-$$
+    VOLATILE LEAKPROOF PARALLEL SAFE
+    AS $$
 DECLARE
-    alphabetArray  text[];
+    alphabetArray text[];
     alphabetLength int := 64;
-    mask           int := 63;
-    step           int := 34;
-    finalId        text; -- Variable to hold the final ID with prefix
+    mask int := 63;
+    step int := 34;
+    finalId text;
+    -- Variable to hold the final ID with prefix
 BEGIN
     IF size IS NULL OR size < 1 THEN
         RAISE EXCEPTION 'The size must be defined and greater than 0!';
     END IF;
-
     IF alphabet IS NULL OR length(alphabet) = 0 OR length(alphabet) > 255 THEN
         RAISE EXCEPTION 'The alphabet can''t be undefined, zero or bigger than 255 symbols!';
     END IF;
-
     IF additionalBytesFactor IS NULL OR additionalBytesFactor < 1 THEN
         RAISE EXCEPTION 'The additional bytes factor can''t be less than 1!';
     END IF;
-
     alphabetArray := regexp_split_to_array(alphabet, '');
     alphabetLength := array_length(alphabetArray, 1);
-    mask := (2 << cast(floor(log(alphabetLength - 1) / log(2)) as int)) - 1;
+    mask :=(2 << cast(floor(log(alphabetLength - 1) / log(2)) AS int)) - 1;
     step := cast(ceil(additionalBytesFactor * mask * size / alphabetLength) AS int);
-
     IF step > 1024 THEN
         step := 1024;
     END IF;
-
-    finalId := prefix || nanoid_optimized(size, alphabet, mask, step); -- Prepend the prefix
+    finalId := prefix || nanoid_optimized(size, alphabet, mask, step);
+    -- Prepend the prefix
     RETURN finalId;
 END
 $$;
 
 DROP FUNCTION IF EXISTS nanoid_optimized(int, text, int, int);
-CREATE OR REPLACE FUNCTION nanoid_optimized(
-    size int, 
-    alphabet text, 
-    mask int, 
-    step int 
-)
-    RETURNS text 
+
+CREATE OR REPLACE FUNCTION nanoid_optimized(size int, alphabet text, mask int, step int)
+    RETURNS text
     LANGUAGE plpgsql
-    VOLATILE
-    LEAKPROOF
-    PARALLEL SAFE
-AS
-$$
+    VOLATILE LEAKPROOF PARALLEL SAFE
+    AS $$
 DECLARE
-    idBuilder      text := '';
-    counter        int  := 0;
-    bytes          bytea;
-    alphabetIndex  int;
-    alphabetArray  text[];
-    alphabetLength int  := 64;
+    idBuilder text := '';
+    counter int := 0;
+    bytes bytea;
+    alphabetIndex int;
+    alphabetArray text[];
+    alphabetLength int := 64;
 BEGIN
     alphabetArray := regexp_split_to_array(alphabet, '');
     alphabetLength := array_length(alphabetArray, 1);
-
     LOOP
         bytes := gen_random_bytes(step);
-        FOR counter IN 0..step - 1
-            LOOP
-                alphabetIndex := (get_byte(bytes, counter) & mask) + 1;
-                IF alphabetIndex <= alphabetLength THEN
-                    idBuilder := idBuilder || alphabetArray[alphabetIndex];
-                    IF length(idBuilder) = size THEN
-                        RETURN idBuilder;
-                    END IF;
+        FOR counter IN 0..step - 1 LOOP
+            alphabetIndex :=(get_byte(bytes, counter) & mask) + 1;
+            IF alphabetIndex <= alphabetLength THEN
+                idBuilder := idBuilder || alphabetArray[alphabetIndex];
+                IF length(idBuilder) = size THEN
+                    RETURN idBuilder;
                 END IF;
-            END LOOP;
+            END IF;
+        END LOOP;
     END LOOP;
 END
 $$;
+
